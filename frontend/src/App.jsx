a8ffda77397e77b64ff87dd6587f51f6ff8d3f73
khanhassu <hassansndigitechsndigitech@gmail.com>
2026-05-05 20:41:41 +0530

    Initial commit - Team Task Manager

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import AuthPage from "./pages/AuthPage";
+import DashboardPage from "./pages/DashboardPage";
+import { useAuth } from "./context/AuthContext";
+import React from "react";
+
+export default function App() {
+  const { token, loading } = useAuth();
+  if (loading) return <div className="screen-loader">Loading workspace...</div>;
+  return token ? <DashboardPage /> : <AuthPage />;
+}
