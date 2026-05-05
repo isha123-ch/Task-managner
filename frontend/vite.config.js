70a1eee01c515b733f100b36cf5d2769343d3ff8
Suhana Chaudhary <suhana@email.com>
2026-05-06 02:52:17 +0530

    final fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     preview: {
-        host: true,
+        host: '0.0.0.0',
         port: process.env.PORT,
-        allowedHosts: 'all'
+        allowedHosts: true
     }
 })
